eed7416c21ee68383389616197bac5b935227ebf
Anushka1804 <anushkaagarwal180405@gmail.com>
2025-02-16 00:21:14 +0530

    Initial commit

---
+++ frontend/src/components/homepage/homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './homepage.css'
+import { Grid, TextField, Button, Card, CardContent, Typography } from '@mui/material';
+import { inputFormElements } from './formElments'
+
+export default function App() {
+
+  const margin = { margin: "0 5px" }
+  return (
+    <div className='App'>
+      
+      <button className="bu1"></button>
+      <button className="bu2"></button>
+      
+    </div>
+  );
+}
